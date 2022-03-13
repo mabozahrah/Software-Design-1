82a2783a28a6415ef838700ea766c6f76a437cc1
mabozahrah <83977849+mabozahrah@users.noreply.github.com>
2022-03-13 12:36:27 -0500

    Add files via upload

---
+++ login.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+from . import db #used database but will implement something more sophisticated for next assignment
+#hardcoding the values, no db for this assignment
+
+#password = "abd123"
+#username = "mariam"
+
+
+#validating using db
+class loginuser(db.Model):
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+
+@app.route("/login", methods = ["GET", "POST"])
+  def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != username or request.form['password'] !=                password:
+            error = 'Invalid Login'
+        else:
+            return redirect(url_for('index2.html'))
+    return render_template('index1.html', error=error)
---
+++ profile.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+@app.route("/profile", methods = ["GET", "POST"])
+  def profile():
+    error = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'username' in request.form and 'address' in request.form and 'city' in request.form and 'state' in request.form and 'zipcode' in request.form 
+        username = request.form['username']
+        password = request.form['password']
+        address = request.form['address']
+        city = request.form['city']
+        state = request.form['state']
+        zipcode = request.form['zipcode'] 
+    elif not re.match(r'[A-Za-z0-9]+', username):
+                error = "Invalid Username"
+    return render_template("index1.html", error = error)
+  return redirect(url_for('index.html'))
+    
