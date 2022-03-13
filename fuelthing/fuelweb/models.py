f210f448044216b83b9547ff27a3c354b07518f4
ChuchuMmm <may1021chu@yahoo.com.tw>
2022-03-13 04:15:06 -0500

    python django

--- fuelthing/fuelweb/models.py
+++ fuelthing/fuelweb/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
+class Client(models.Model): 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=5)
+
+class Quote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    address = models.CharField(max_length=100)
+    gallons = models.IntegerField()
+    total_price = models.DecimalField(max_digits=20, decimal_places=2)
