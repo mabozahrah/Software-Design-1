7938f0ce3cd02af756b8b247bff42b245a9720b1
ChuchuMmm <may1021chu@yahoo.com.tw>
2022-03-14 01:46:20 -0500

    assignment 3

---
+++ fuelthing/app.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+user_info = {
+    'username': 'admin',
+    'password': 'secret-password'
+}
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username, password)
+        if username == user_info['username'] and password == user_info['password']:
+            print('login successful')
+            print(username, password)
+            return jsonify({'success': 'login successful'}), 200
+        else:
+            return jsonify({'error': 'Invalid credentials'}), 400
+    return render_template('login.html'), 200
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        address1 = request.form.get("address1")
+        address2 = request.form.get("address2")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        zipcode = request.form.get("zipcode")
+        print(name, address1, address2, city, state, zipcode)
+        if name == '':
+            return jsonify({"error": "A name is required"}), 400
+        if len(name) > 100:
+            return jsonify({"error": "Name can only be up to 150 characters."}), 400
+
+        if address1 == '':
+            return jsonify({"error": "Address is required"}), 400
+        if len(address1) > 100 or len(address1) <= 0:
+            return jsonify({"error": "Address 1 can only be up to 100 characters"}), 400
+
+        if len(address2) > 100:
+            return jsonify({"error": "Address 2 can only be up to 100 characters"}), 400
+
+        if len(city) > 100 or len(city) <= 0:
+            return jsonify({"error": "city is required & can only be up to 100 characters"}), 400
+
+        if state == "":
+            return jsonify({"error": "state is required"}), 400
+
+        if len(zipcode) > 8 or len(zipcode) < 5:
+            return jsonify({"error": "zipcode is required & must be between 5-8 characters"}), 400
+
+        print(name, address1, address2, city, state, zipcode)
+        return jsonify({"success": "Profile Updated"}), 200
+    return render_template('profile.html'), 200
+
+
+@app.route('/fuel-quote', methods=['GET', 'POST'])
+def fuel_quote():
+    if request.method == 'POST':
+        gallons = request.form.get('gallons')
+        delivery_date = request.form.get('delivery_date')
+
+        print(gallons, delivery_date)
+        if gallons == '':
+            return jsonify({"error": "Gallons is required"}), 400
+        if gallons == '' and delivery_date is None:
+            return jsonify({"error": "Both field is required"}), 400
+        if int(gallons) <= 0:
+            return jsonify({"error": "Gallons must be greater than 0"}), 400
+
+        if not delivery_date:
+            return jsonify({"error": "Delivery date is required"}), 400
+        print(gallons, delivery_date)
+        return jsonify({"success": "Fuel Quote Submitted"}), 200
+    return render_template('quote.html'), 200
+
+
+@app.route('/history')
+def quote_history():
+    return render_template('quote-history.html'), 200
+
+
+class Pricing:
+    def __init__(self, gallons, delivery_address, delivery_date):
+        self.gallons = gallons
+        self.delivery_address = delivery_address
+        self.delivery_date = delivery_date
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ fuelthing/test_app.py
@@ -0,0 +1,180 @@
+import pytest
+from app import app
+
+"""---------- coverage: platform linux, python 3.8.10-final-0 -----------
+Name          Stmts   Miss  Cover
+---------------------------------
+test_app.py      75      0   100%
+---------------------------------
+TOTAL            75      0   100%
+"""
+
+
+class TestLogin:
+
+    @pytest.fixture
+    def client(self):
+        return app.test_client()
+
+    def test_get_login(self, client):
+        resp = client.get('/login')
+        assert resp.status_code == 200
+
+    def test_login_with_correct_data(self, client):
+        response = client.post("/login", data={
+            "username": "admin",
+            "password": "secret-password"
+        })
+        assert response.status_code == 200
+        assert response.json.get('success')
+
+    def test_login_with_fake_data(self, client):
+        resp = client.post('/login', data={
+            "username": "user",
+            "password": "password"
+        })
+        assert resp.status_code == 400
+        assert not resp.json.get('success')
+
+
+class TestProfile:
+
+    @pytest.fixture
+    def client(self):
+        return app.test_client()
+
+    def test_get_profile(self, client):
+        resp = client.get('/profile')
+        assert resp.status_code == 200
+
+    def test_correct_form(self, client):
+        response = client.post("/profile", data={
+            "name": "admin",
+            "address1": "3646 Bates Brothers Road",
+            "address2": "3646 Bates Brothers Road",
+            "city": "Columbus",
+            "state": "Ohio",
+            "zipcode": "43215"
+        })
+        assert response.status_code == 200
+        assert response.json.get('success')
+
+    def test_data_without_optional(self, client):
+        response = client.post("/profile", data={
+            "name": "admin",
+            "address1": "3646 Bates Brothers Road",
+            "address2": "",
+            "city": "Columbus",
+            "state": "Ohio",
+            "zipcode": "43215"
+        })
+        assert response.status_code == 200
+        assert response.json.get('success')
+
+    def test_name_required_field(self, client):
+        response = client.post("/profile", data={
+            "name": "",
+            "address1": "3646 Bates Brothers Road",
+            "address2": "3646 Bates Brothers Road",
+            "city": "Columbus",
+            "state": "Ohio",
+            "zipcode": "43215"
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+    def test_address1_required_field(self, client):
+        response = client.post("/profile", data={
+            "name": "admin",
+            "address1": "",
+            "address2": "3646 Bates Brothers Road",
+            "city": "Columbus",
+            "state": "Ohio",
+            "zipcode": "43215"
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+    def test_city_required_field(self, client):
+        response = client.post("/profile", data={
+            "name": "admin",
+            "address1": "3646 Bates Brothers Road",
+            "address2": "3646 Bates Brothers Road",
+            "city": "",
+            "state": "Ohio",
+            "zipcode": "43215"
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+    def test_zipcode_length(self, client):
+        response = client.post("/profile", data={
+            "name": "admin",
+            "address1": "3646 Bates Brothers Road",
+            "address2": "",
+            "city": "Columbus",
+            "state": "Ohio",
+            "zipcode": "5"
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+
+class TestFuelQuote:
+
+    @pytest.fixture
+    def client(self):
+        return app.test_client()
+
+    def test_index(self, client):
+        resp = client.get('/fuel-quote')
+        assert resp.status_code == 200
+
+    def test_gallons_empty_field(self, client):
+        response = client.post("/fuel-quote", data={
+            "gallons": ""
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+    def test_gallons_zero_form(self, client):
+        response = client.post("/fuel-quote", data={
+            "gallons": "0"
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+    def test_delivery_date_empty(self, client):
+        response = client.post("/fuel-quote", data={
+            "gallons": "5",
+            "delivery_date": None
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+    def test_both_empty_form(self, client):
+        response = client.post("/fuel-quote", data={
+            "gallons": "",
+            "delivery_date": None
+        })
+        assert response.status_code == 400
+        assert response.json.get('error')
+
+    def test_both_valid_form(self, client):
+        response = client.post("/fuel-quote", data={
+            "gallons": "5",
+            "delivery_date": "2055-03-15"
+        })
+        assert response.status_code == 200
+        assert response.json.get('success')
+
+
+class TestFuelQuoteHistory:
+
+    @pytest.fixture
+    def client(self):
+        return app.test_client()
+
+    def test_get_fuel_quote_history(self, client):
+        resp = client.get('/history')
+        assert resp.status_code == 200
